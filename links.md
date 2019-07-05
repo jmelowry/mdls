a6e55a04ceed14d264ca5c2c6b14ad549065f24f
Jamie Lowry <jamie.lowry@airbnb.com>
2019-07-04 17:52:11 -0700

    writing the file

--- links.md
+++ links.md
@@ -4,3 +4,5 @@ Resources I've used to get going.
 
 - https://golang.org/doc/code.html
 - https://github.com/avelino/awesome-go
+- https://gobyexample.com/command-line-arguments
+- https://golangbot.com/write-files/
--- mkls.go
+++ mkls.go
@@ -6,6 +6,26 @@ import (
 )
 
 func main() {
-	allArgs := os.Args[1:]
-	fmt.Println(allArgs)
+	f, err := os.Create("list.md")
+	if err != nil {
+		fmt.Println(err)
+		f.Close()
+		return
+	}
+
+	d := []string{"This is my simple(?) list."}
+
+	for _, v := range d {
+		fmt.Fprintln(f, v)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+	}
+	err = f.Close()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("list created?")
 }
